bb7e5243bd3dcb242e3e2a77ec48eeb488261381
dabuchho <daniel.buchholzer1337@gmail.com>
2024-04-04 13:21:36 +0200

    Change struct

--- src/conf.jl
+++ src/conf.jl
@@ -1,4 +1,31 @@
 # configuration
+export 
+    Scale, 
+    ConfSize, 
+    torsions_set_to_null!, # could rename torsions_ methods because multiple dispatch
+    torsions_increment!,
+    torsions_randomize!,
+    torsions_too_close,
+    torsions_generate!,
+    RigidChange,
+    RigidConf,
+    set_to_null!,
+    increment!,
+    randomize!,
+    too_close,
+    mutate_position!,
+    mutate_orientation!,
+    generate!,
+    apply!,
+    LigandChange,
+    LigandConf,
+    ResidueChange,
+    ResidueConf,
+    Change,
+    operator,
+    Conf
+
+
 
 struct Scale{T<:AbstractFloat}
     position::T       
@@ -8,15 +35,21 @@ end
 
 # typedef std::size_t sz;           # => size of any object
 # typedef std::vector<sz> szv;      # => vector of object sizes ? 
-struct ConfSize              # TODO: option between Int64 and Int32
+struct ConfSize              
     ligands::Vector{Int64}   # AutoDock typed this szv (number of elements in a vector)
     flex::Vector{Int64}      # szv
     degrees_of_freedom::Int64
     function ConfSize(ligands, flex)
-        new(ligands,
-            flex,
-            sum(ligands) + sum(flex) + 6*length(ligands) # ?
-        ) 
+        # 6 * number_of_atoms, because 3 translational and 3 rotational degrees
+        # allow en empty ConfSize for now
+        if length(ligands) == 0 || length(flex) == 0
+            new(ligands, flex, 0)
+        else
+            new(ligands,
+                flex,
+                sum(ligands) + sum(flex) + 6*length(ligands)
+            ) 
+        end
     end
 end         # this could be combined with BALL
 
@@ -62,11 +95,11 @@ function torsions_generate!(
     torsions::Vector{T}, 
     spread::T, 
     rp::T, 
-    rs::Vector{T}       # rs as kwargs... ?
+    rs::Union{Vector{T}, Nothing} = nothing   # rs does not need to exist. TEST THIS!
 ) where T
-    @assert size(rs) == size(torsions)    # rs not always present => add nothing condition?
+    @assert isnothing(rs) || size(rs) == size(torsions) # if rs resent, check size
     for i in eachindex(torsions)
-        if rand(T) < rp                     # rs condition
+        if !isnothing(rs) && rand(T) < rp
             torsions[i] = rs[i]
         else
             torsions[i] += rand(T) * 2*spread - spread
@@ -74,13 +107,12 @@ function torsions_generate!(
     end
 end     # add Seed/ RNG option
 
-
 struct RigidChange{T<:AbstractFloat}
     position::Vector{T}
     orientation::Vector{T}
 end
 
-RigidChange{T}() where T = RigidChange(T[0,0,0], T[0,0,0])  # is this bad practice?
+RigidChange{T}() where T = RigidChange(T[0,0,0], T[0,0,0])
 
 
 mutable struct RigidConf{T<:AbstractFloat}
@@ -89,7 +121,7 @@ mutable struct RigidConf{T<:AbstractFloat}
 end
 
 RigidConf{T}() where T = RigidConf(
-    Vector{T}(0,0,0), 
+    Vector{T}([0,0,0]), 
     Quaternion{T}(1,0,0,0)     # identity Quaternion
 )
 
@@ -133,34 +165,162 @@ function too_close(
 end
 
 
-struct LigandChange
-    rigid::RigidChange
-    torsions::AbstractVector
+function mutate_position!(rconf::RigidConf{T}, spread::T) where T
+    rconf.position += spread * random_inside_sphere(spread)
 end
 
-struct LigandConf
-    rigid::RigidChange
-    torsions::AbstractVector
+
+function mutate_orientation!(rconf::RigidConf{T}, spread::T) where T
+    tmp = spread * random_inside_sphere(T)
+    quaternion_increment(rconf.orientation, tmp)
 end
 
-# ...
 
-struct ResidueChange
-    torsions::AbstractVector
+function generate!(
+    rconf::RigidConf{T}, 
+    position_spread::T, 
+    orientation_spread::T,
+    rp::T,
+    rs::Union{RigidConf{T}, Nothing} = nothing  # might be slow
+) where T
+    if !isnothing(rs) && rand(T) < rp
+        rconf.position = rs.position
+    else
+        mutate_position!(rconf.position, position_spread)
+    end
+    if !isnothing(rs) && rand(T) < rp
+        rconf.orientation = rs.orientation
+    else
+        mutate_orientation!(rconf.orientation, orientation_spread)
+    end
 end
 
-# ...
-
-struct Change
-    ligands::Vector{LigandChange}
-    flex::Vector{ResidueChange}
+# apply orientation of RigidConf on in::Vector to receive out::Vector
+function apply!(
+    rconf::RigidConf{T}, 
+    in::Vector{T}, # const
+    out::Vector{T},
+) where T
+    @assert size(in) == size(out)
+    m = rotation_from_quaternion(rconf.orientation)
+    mul!(out, m, in) + rconf.position
 end
 
-# ...
 
-struct Conf
-    ligands::Vector{LigandChange}
-    flex::Vector{ResidueChange}
+struct LigandChange{T<:AbstractFloat}
+    rigid::RigidChange{T}
+    torsions::Vector{T}
+end
+
+
+struct LigandConf{T<:AbstractFloat}
+    rigid::RigidConf{T}
+    torsions::Vector{T}
+end
+
+
+function set_to_null!(lconf::LigandConf{T}) where T
+    set_to_null!(lconf.rigid)
+    torsions_set_to_null!(lconf.torsions)
+end
+
+
+function increment!(
+    lconf::LigandConf{T}, 
+    lchan::LigandChange{T},
+    factor::T
+) where T
+    increment!(lconf.rigid, lchan.rigid, factor)
+    torsions_increment!(lconf.torsions, lchan.torsions, factor)
+end
+
+
+function randomize!(
+    lconf::LigandConf{T}, 
+    corner1::Vector{T}, 
+    corner2::Vector{T}
+) where T
+    randomize!(lconf.rigid, corner1, corner2)
+    torsions_randomize!(lconf.torsions)
+end
+
+
+struct ResidueChange{T<:AbstractFloat}
+    torsions::Vector{T}
+end
+
+
+struct ResidueConf{T<:AbstractFloat}
+    torsions::Vector{T}
+end
+
+
+function set_to_null!(rconf::ResidueConf{T}) where T
+    torsions_set_to_null!(rconf.torsions)
+end
+
+
+function increment!(
+    rconf::ResidueConf{T}, 
+    rchan::ResidueChange{T},
+    factor::T
+) where T
+    torsions_increment!(rconf.torsions, rchan.torsions, factor)
+end
+
+
+function randomize!(rconf::ResidueConf{T}) where T
+    torsions_randomize!(rconf.torsions)
+end
+
+
+struct Change{T<:AbstractFloat}
+    ligands::Vector{LigandChange{T}}
+    flex::Vector{ResidueChange{T}}
+end
+
+
+# This is super ugly. This constructor, when called, also always creates 
+# a new LigandChange and RigidChange object, which is not ideal.
+function Change(s::ConfSize, type::T=Float64) where T
+    # first, initialize a vector of the size of the ConfSize
+    ligands = Vector{LigandChange{type}}(undef, length(s.ligands))
+    flex = Vector{ResidueChange{type}}(undef, length(s.flex))
+    
+    # then, resize ligands and flex according to each element in ConfSize.ligands
+    # and Confsize.flex
+    for i in eachindex(ligands)
+        ligands[i] = LigandChange(RigidChange{type}(), fill(type(0), s.ligands[i]))
+    end
+    
+    for i in eachindex(flex)
+        flex[i] = ResidueChange(fill(type(0), s.flex[i]))
+    end
+    
+    Change(ligands, flex)
+end
+
+
+function operator(c::Change, index::T) where T
+    for i in eachindex(c.ligands)
+        lig = c.ligands[i]
+        if index < 3 return lig.rigid.position[index] end
+        index -= 3
+        if index < 3 return lig.rigid.position[index] end
+        index -= 3
+        if index < length(lig.torsions) return lig.torsions[index] end
+        index -= length(lig.torsions)
+    end
+    for i in eachindex(c.flex)
+        res = c.flex[i]
+        if index < length(res.torsions) return res.torsions[index] end
+        index -= length(res.torsions)
+    end
+end
+
+struct Conf{T<:AbstractFloat}
+    ligands::Vector{LigandChange{T}}
+    flex::Vector{ResidueChange{T}}
 end
 
 # ...
@@ -177,3 +337,25 @@ struct OutputType
     total
     coords
 end
+
+
+# Vectors of position, orientation, torsions
+pos = Float64[]
+ori = Float64[]
+tor = Float64[]
+rchan = RigidChange{Float64}(pos, ori)
+lchan = LigandChange{Float64}(rchan, tor)
+vlc = Vector{LigandChange{Float64}}([lchan])
+vrc = Vector{ResidueChange{Float64}}(tor)
+
+# Change(vlc, vrc) # unfinished
+
+# Vectors of ligands and flex
+lig = [1,2,3,4,5]
+flex = [1,1,1]
+cs = ConfSize(lig, flex)
+
+change = Change(cs)
+
+
+Vector{LigandChange{Float64}}()
