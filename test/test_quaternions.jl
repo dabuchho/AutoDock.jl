09146f135db56794a41997e6e0d9a461fd6944ed
dabuchho <daniel.buchholzer1337@gmail.com>
2024-04-26 21:41:59 +0200

    started testing

--- test/test_quaternions.jl
+++ test/test_quaternions.jl
@@ -1,5 +1,6 @@
 @testitem "Quaternions" begin
     using Quaternions
+    using LinearAlgebra
     using Rotations
     # type tests    
     for T in [Float32, Float64]
@@ -27,6 +28,49 @@
         @test typeof(quaternion_difference(q,q2)) == Vector{T}
 
         # correctness
-        # ...
+        IQ = Quaternion{T}(1,0,0,0)  # identity Quaternion
+        v₀ = zeros(T, 3)             # zero Vector
+        ϑ  = rand(T)                 # random angle
+        
+        # TODO: merge type tests with correctness tests
+        @test qt_identity(T) == IQ
+        #= Consider writing a test that compares "random" Quaternions (seeded)
+           generate_quaternion() == random_quaternion()
+        =#
+        r = rand(T, 4)
+        q = generate_quaternion(r[1], r[2], r[3], r[4])
+
+        @test quaternion_norm_sqr(q) == sum(r.*r)
+        q_normalized = quaternion_normalize(q)
+        @test length(q_normalized) == 1
+        quaternion_is_normalized(q_normalized) == true  # testing this twice
+        
+        @test quaternion_from_angle(v₀)    == IQ
+        @test quaternion_from_angle(v₀, ϑ) == IQ
+        
+        # TODO: predict outcome (for a given set of parameters for example)
+        v = rand(T, 3)
+        @test quaternion_from_angle(v, ϑ) != IQ
+        @test quaternion_from_angle(v)    != IQ
+        
+        v_small = T[eps(T) * T(0.1), 0, 0]    # norm(v_small) < ε
+        @test quaternion_from_angle(v_small) == IQ
+
+        @test vector_from_quaternion(IQ)           == v₀  
+        @test vector_from_quaternion(q_normalized) != v₀ 
+
+        @test rotation_from_quaternion(IQ) == Matrix(I, 3, 3)
+        @test rotation_from_quaternion(q)  != Matrix(I, 3, 3)
+        # random_quaternion() 
+        # quaternion_increment() 
+        # quaternion_dfference()
     end
 end
+
+using Quaternions
+using LinearAlgebra
+using Rotations
+
+# I = Quaternion{Float32}(1,0,0,0)
+# q = generate_quaternion(1,0,0,0)
+# QuatRotation{Float32}(q) == Matrix(I, 3, 3)
